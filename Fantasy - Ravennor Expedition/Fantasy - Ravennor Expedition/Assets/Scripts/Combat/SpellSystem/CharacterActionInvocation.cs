efae391d8976f805cfa8b640568c880af6dc418e
t.lafranche <linkazer@gmail.com>
2021-02-01 09:28:11 +0100

    On tente un truc

---
+++ Fantasy - Ravennor Expedition/Fantasy - Ravennor Expedition/Assets/Scripts/Combat/SpellSystem/CharacterActionInvocation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Invocation Spell", menuName = "Spell/Create Invocation Spell")]
+public class CharacterActionInvocation : CharacterActionScriptable
+{
+    public PersonnageScriptables invocation;
+
+    public CharacterActionInvocation()
+    {
+        spellType = SpellType.Invocation;
+    }
+}
